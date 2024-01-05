470cb3e624899b958c814c551ec32695e0f738f9
Richard Roxas <chardsourcex@gmail.com>
2024-01-05 18:59:37 +0800

    initial commit

---
+++ components/index.js
@@ -0,0 +1,9 @@
+import About from './About'
+import ContactUs from './ContactUs'
+import Footer from './Footer'
+import Hero from './Hero'
+import Nav from './Nav'
+import Services from './Services'
+import Testimonials from './Testimonials'
+
+export { About, ContactUs, Footer, Hero, Nav, Services, Testimonials }
